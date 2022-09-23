e91a380ffe64196da2e56cd377427e577f2216ec
sebseb7 <sebgreenbus@gmail.com>
2022-09-24 00:36:00 +0200

    upd

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import {
 } from 'react-native';
 
 import Geolocation from '@react-native-community/geolocation';
+import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
 var uniqueId = uuidv4();
